646dfe89b473eab9a57fc9cd1758b24f41a40ea6
Ritam Haldar <rickhaldar561@gmail.com>
2026-01-22 20:40:06 +0530

    Day2

---
+++ Day2/index.js
@@ -0,0 +1,12 @@
+const express=require("express")
+const app=express();
+app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+app.get("/about",(req,res)=>{
+    res.send("This is about page")
+})
+app.get("/home",(req,res)=>{
+    res.send("This is home page")
+})
+app.listen(3000);
