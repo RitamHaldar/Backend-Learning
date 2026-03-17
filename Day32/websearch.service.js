a6c7bc5bd08216b4eac1f8fac47e2821930bdfe7
Ritam Haldar <rickhaldar561@gmail.com>
2026-03-17 23:14:06 +0530

    feat: add web search service using Tavily API

--- Day32/websearch.service.js
+++ Day32/websearch.service.js
@@ -1,8 +1,11 @@
 import { tavily } from "@tavily/core"
 export async function websearch({ query }) {
     const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
-    const response = await tvly.search(query);
-    console.log(response.results)
+    const response = await tvly.search(query, {
+        maxResults: 1,
+        searchDepth: "basic",
+        timeRange: "day"
+    });
     return response.results[0].content;
 
 }
